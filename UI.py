40000b9fd9959d9d4ede6b556fbed4788dfede63
Efe Yencilek <efefrk30@gmail.com>
2024-03-05 16:56:51 -0500

    installed customtkinter for design. Monitor proprety in App needs directory input, solve the issue by either changing directory input requirement or placement of the monitor object as property or instance.

--- UI.py
+++ UI.py
@@ -4,50 +4,52 @@
 from Monitor import Monitor
 from Subprocess import Subprocess
 import tkinter as tk
+import customtkinter as ctk
 from tkinter import ttk
 from Helpers import *
 from threading import Thread,Event
 
 # This custom button class will be used for better design, abstraction and specialized use of buttons on the window
-class Button(ttk.Button):
-    def __init__(self,window:ttk.Frame,txt:str,call:callable):
+class CustomButton(ctk.CTkButton):
+    def __init__(self,window:ctk.CTkFrame,txt:str,call:callable):
         super().__init__(window,text=txt,command=call)
-    
-    def display(self):
-        # Make button layers and pack them
-        pass
+        self.place(relx=0.5,rely=0.5,width=120,relheight=30,anchor='center')
 
 class App:
 
     monitor = Monitor() # This class starts monitoring the downloads
     process = Subprocess() # This class opens a file dialog for directory selection
-    event = Event()
+
 
     def __init__(self):
-        self.window = tk.Tk()
+        self.window = tk.Tk() # Main window
+        self.event = Event() # Handle monitor thread
+        # Tk window specs
         self.window.title("Downloads Classifier")
         self.window.geometry(f"{WIDTH}x{HEIGHT}")
-        self.button_frame = ttk.Frame(self.window)
-        self.buttons: list[ttk.Button] = []
     
     # View Layout
-    def __gen_buttons(self):
+    def __gen_frame(self) -> ctk.CTkFrame:
+        button_frame = ctk.CTkFrame(master=self.window)
+        button_frame.place(relx=0.1,rely=0.5,relwidth=0.2,relheight=1.0,anchor='center')
+        button_frame.pack()
+        return button_frame
+    
+    def __gen_buttons(self,frame):
         # Generate buttons, input fields & assign tasks
-        button_dir = ttk.Button(master=self.button_frame,text="Choose Downloads Folder",command=lambda x: x+1)
-        button_dest = ttk.Button(master=self.button_frame,text="Choose Destination For Your Files",command= lambda x: x+1)
-        button_start = ttk.Button(master=self.button_frame,text="Start",command=lambda x:x+1)
-        button_stop = ttk.Button(master=self.button_frame,text="Stop",command=self.__kill_thread)
-        button_dir.pack()
-        button_dest.pack()
-        button_start.pack()
-        button_stop.pack()
-        self.buttons = [button_dir,button_dest,button_start,button_stop]
+        button_dir = ttk.Button(master=frame,text="Choose Downloads Folder",command=lambda x: x+1)
+        button_dest = ttk.Button(master=frame,text="Choose Destination For Your Files",command= lambda x: x+1)
+        button_start = ttk.Button(master=frame,text="Start",command=self.__run_monitor)
+        button_stop = ttk.Button(master=frame,text="Stop",command=self.__kill_thread)
+        button_dir.pack(pady=10)
+        button_dest.pack(pady=10)
+        button_start.pack(pady=10)
+        button_stop.pack(pady=10)
     
     # Back-end Operations
     def __kill_thread(self):
         self.event.set()
-    # Before this code runs, you have to make sure that files have a destination
-    # The code wouldn't crash either way, but why run something when it serves no purpose
+
     def __monitor_mainloop(self,event:Event):
         self.monitor.start()
         try:
@@ -67,8 +69,10 @@ class App:
         finally:
             self.monitor.stop()
             self.monitor.join(0)
-    
-    def runMonitor(self):
+            
+    # Before this code runs, you have to make sure that files have a destination
+    # The code wouldn't crash either way, but why run something when it serves no purpose
+    def __run_monitor(self):
         t = Thread(target=self.__monitor_mainloop,args=(self.event,))
         t.start()
         
@@ -76,7 +80,8 @@ class App:
 
     def runApp(self):
         # Here put any previous setup to do before running the app mainloop
-        self.__gen_buttons()
+        frame = self.__gen_frame()
+        self.__gen_buttons(frame)
         # This is the app mainloop
         self.window.mainloop()
 
